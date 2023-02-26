5cc028f893c8ddd65b5ff3fc9af145ae46c0c48a
AnnijaVitolina <55869202+AnnijaVitolina@users.noreply.github.com>
2023-02-27 00:10:24 +0200

    Add files via upload

---
+++ Webflow_sentiment.py
@@ -0,0 +1,50 @@
+import boto3
+import json
+import pandas as pd
+
+comprehend = boto3.client(service_name='comprehend', region_name='eu-central-1')
+df = pd.read_excel('/Users/annijavitolina/Desktop/uni/bachelor/Scraped/Trustpilot/Trustpilot-Webflow-filtered.xlsx')
+
+out_file = open("/Users/annijavitolina/Desktop/uni/bachelor/Scraped/Trustpilot/Webflow/Webflow_sentiment.xlsx", "w")
+all_jsns = []
+
+data_dict = {'content-length':[], 'date':[], 'Sentiment':[], 'Mixed':[], 'Negative':[], 'Neutral':[], 'Positive':[]}
+
+
+for index, row in df.iterrows():
+    print(row[0])
+
+    row_str = str(row[0])
+    DetectSentimenttext = row_str
+    print('Calling DetectSentiment')
+    sentiment = json.dumps(comprehend.detect_sentiment(Text=DetectSentimenttext, LanguageCode='en'), sort_keys=True, indent=4)
+    print(sentiment)
+    print('End of DetectSentiment\n')
+    #out_file.write(sentiment)
+
+    sentiment = json.loads(sentiment)
+    
+    # Put info in dictionary
+
+    data_dict['content-length'].append(sentiment['ResponseMetadata']['HTTPHeaders']['content-length'])
+    data_dict['date'].append(sentiment['ResponseMetadata']['HTTPHeaders']['date'])
+    data_dict['Sentiment'].append(sentiment['Sentiment'])
+    data_dict['Mixed'].append(sentiment['SentimentScore']['Mixed'])
+    data_dict['Negative'].append(sentiment['SentimentScore']['Negative'])
+    data_dict['Neutral'].append(sentiment['SentimentScore']['Neutral'])
+    data_dict['Positive'].append(sentiment['SentimentScore']['Positive'])
+
+    # Convert the JSON data to a Pandas DataFrame
+    #df1 = pd.DataFrame.from_dict(comprehend.detect_sentiment(Text=DetectSentimenttext, LanguageCode='en'), sort_keys=True, indent=4)
+
+    # Write the DataFrame to an Excel file
+    #df1.to_excel("/Users/annijavitolina/Desktop/uni/bachelor/Scraped/Trustpilot/json.xlsx", index=False)
+
+# Create a dataframe
+df1 = pd.DataFrame.from_dict(data_dict)
+
+# Export to Excel
+df1.to_excel('/Users/annijavitolina/Desktop/uni/bachelor/Scraped/Trustpilot/Webflow/Webflow_sentiment.xlsx')
+#out_file.write(large_json) 
+#out_file.write(json.dumps(all_jsns))
+
---
+++ Webflow_sentiment_capterra.py
@@ -0,0 +1,36 @@
+import boto3
+import json
+import pandas as pd
+
+comprehend = boto3.client(service_name='comprehend', region_name='eu-central-1')
+df = pd.read_excel('/Users/annijavitolina/Desktop/uni/bachelor/Scraped/Capterra/Webflow/Capterra-webflow-filtered.xlsx')
+
+data_dict = {'content-length':[], 'date':[], 'Sentiment':[], 'Mixed':[], 'Negative':[], 'Neutral':[], 'Positive':[]}
+
+print('Calling DetectSentiment')
+for index, row in df.iterrows():
+    
+    #print(row[0])
+    DetectSentimenttext = str(row[0])
+    sentiment = json.dumps(comprehend.detect_sentiment(Text=DetectSentimenttext, LanguageCode='en'), sort_keys=True, indent=4)
+    #print(sentiment)
+    sentiment = json.loads(sentiment)
+    
+    # Put info in dictionary
+
+    data_dict['content-length'].append(sentiment['ResponseMetadata']['HTTPHeaders']['content-length'])
+    data_dict['date'].append(sentiment['ResponseMetadata']['HTTPHeaders']['date'])
+    data_dict['Sentiment'].append(sentiment['Sentiment'])
+    data_dict['Mixed'].append(sentiment['SentimentScore']['Mixed'])
+    data_dict['Negative'].append(sentiment['SentimentScore']['Negative'])
+    data_dict['Neutral'].append(sentiment['SentimentScore']['Neutral'])
+    data_dict['Positive'].append(sentiment['SentimentScore']['Positive'])
+
+print('End of DetectSentiment\n')
+
+# Create a dataframe
+df1 = pd.DataFrame.from_dict(data_dict)
+
+# Export to Excel
+df1.to_excel('/Users/annijavitolina/Desktop/uni/bachelor/Scraped/Capterra/Webflow/Capterra_We_sentiment.xlsx')
+
